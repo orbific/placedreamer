6682916f02d06d34887ba33131a4e9a383b63c10
James Burt <james@orbific.com>
2017-07-11 14:07:53 +0100

    Added new intents and handling for them

--- src/main/java/com/riddlefox/placedreamer/PlacedreamerSpeechlet.java
+++ src/main/java/com/riddlefox/placedreamer/PlacedreamerSpeechlet.java
@@ -2,13 +2,13 @@ package com.riddlefox.placedreamer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.SessionEndedRequest;
 import com.amazon.speech.speechlet.SessionStartedRequest;
-import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.speechlet.SpeechletV2;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
@@ -17,6 +17,7 @@ import com.amazon.speech.ui.SimpleCard;
 public class PlacedreamerSpeechlet implements SpeechletV2 {
 
 	private static final Logger log = LoggerFactory.getLogger(PlacedreamerSpeechlet.class);
+	private static final String TITLE = "Travel Stories";
 	
 	@Override
 	public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> requestEnvelope) {
@@ -24,9 +25,9 @@ public class PlacedreamerSpeechlet implements SpeechletV2 {
 
 	@Override
 	public SpeechletResponse onLaunch(SpeechletRequestEnvelope<LaunchRequest> requestEnvelope) {
-		String speechText = "Welcome to Placedreamer";
+		String speechText = "Welcome to Travel Stories. Simply say \"Travel Stories, describe a place\".";
 		PlainTextOutputSpeech speech = createSpeechPhrase(speechText);
-		SimpleCard alexaCard = createAlexaCard("Placedreamer", speechText);
+		SimpleCard alexaCard = createAlexaCard(TITLE, speechText);
 		return SpeechletResponse.newTellResponse(speech, alexaCard);
 	}
 
@@ -36,11 +37,28 @@ public class PlacedreamerSpeechlet implements SpeechletV2 {
 		String intentName = (intent != null) ? intent.getName() : null;
 		if ("DescribePlaceIntent".equals(intentName)) {
 			return getSinglePlaceResponse();
+		} else if ("AMAZON.CancelIntent".equals(intent.getName()) || "AMAZON.StopIntent".equals(intent.getName())) {
+	        return getExitIntentResponse();
+		} else if ("AMAZON.HelpIntent".equals(intent.getName())) {
+	        return getHelpIntentResponse();
 		} else {
-			// TODO how would this ever get called?
 			log.error("Intent {} not recognized for requestId {}", intentName, requestEnvelope.getRequest().getRequestId());
-      		throw new RuntimeException("Invalid Intent");
+	  		throw new RuntimeException("Invalid Intent");
 		}
+	}
+
+	private SpeechletResponse getHelpIntentResponse() {
+		String speechText = "Travel Stories describes locations you might want to visit. Simply ask Travel Stories to describe a place.";
+		SimpleCard card = createAlexaCard(TITLE, speechText);
+		PlainTextOutputSpeech speech = createSpeechPhrase(speechText);
+		return SpeechletResponse.newTellResponse(speech, card);
+	}
+
+	private SpeechletResponse getExitIntentResponse() {
+		String speechText = "Good-Bye.";
+		SimpleCard card = createAlexaCard(TITLE, speechText);
+		PlainTextOutputSpeech speech = createSpeechPhrase(speechText);
+		return SpeechletResponse.newTellResponse(speech, card);
 	}
 
 	@Override
@@ -53,7 +71,7 @@ public class PlacedreamerSpeechlet implements SpeechletV2 {
 		String speechText = placedreamer.getPlaceDescription();
 
 		// Create the Simple card content.
-		SimpleCard card = createAlexaCard("Placedreamer", speechText);
+		SimpleCard card = createAlexaCard(TITLE, speechText);
 		PlainTextOutputSpeech speech = createSpeechPhrase(speechText);
 		return SpeechletResponse.newTellResponse(speech, card);
 	}
